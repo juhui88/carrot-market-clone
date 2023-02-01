6665b8541da04a55076714bb00831d3cd24e20db
juhui88 <sand8594@hufs.ac.kr>
2023-02-01 18:52:52 +0900

    update stream detail

--- carrot-market/pages/streams/index.tsx
+++ carrot-market/pages/streams/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Live:NextPage = () => {
     return <div className="py-10 space-y-4 divide-y-2">
         {[...Array(5).fill(1).map((_,i) => (
-        <div className = "pt-4 px-4"key = {i}>
+        <Link href={`/streams/${i}`}key = {i}>
+        <div className = "pt-4 px-4">
             <div className="w-full rounded-md bg-slate-300 aspect-video"/>
             <h3 className=" text-gray-700 text-lg mt-2">Let&apos;s try potatos</h3>
-        </div>    
+        </div>
+        </Link>
+            
         ))]}
         <button className="p-4 shadow-xl hover:bg-orange-500 bg-orange-400 text-white rounded-full fixed right-3 bottom-5 transition-colors">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
