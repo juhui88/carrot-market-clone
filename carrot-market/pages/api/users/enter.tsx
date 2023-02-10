34a662a7a93bb2fb70b6d5b0e4bbbc4f25d2d035
juhui88 <sand8594@hufs.ac.kr>
2023-02-10 15:39:48 +0900

    fix response type

--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -1,14 +1,15 @@
 
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/client/client";
 
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
   const token = await client.token.create({
@@ -62,7 +63,9 @@ async function handler(
     }
   } */
 
-  return res.status(200).end();
+  return res.json({
+    ok:true,
+  })
 }
 
 export default withHandler("POST", handler);
