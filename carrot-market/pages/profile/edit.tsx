14f17bf2d1a30c62505bab6a545e23c75de06ec9
juhui88 <sand8594@hufs.ac.kr>
2023-02-13 21:23:32 +0900

    fix 오류사항 (route)

--- carrot-market/pages/profile/edit.tsx
+++ carrot-market/pages/profile/edit.tsx
@@ -1,6 +1,7 @@
-import Button from "@/components/button";
-import Input from "@/components/input";
-import Layout from "@/components/layout";
+import Button from "@components/button";
+import Input from "@components/input";
+import Layout from "@components/layout";
+
 import type { NextPage } from "next";
 
 const EditProfile: NextPage = () => {
