b3fed2038c49cdab499a8f01262eefc2cd65c8ec
juhui88 <sand8594@hufs.ac.kr>
2023-02-16 23:13:50 +0900

    update 좋아요 기능

---
+++ carrot-market/pages/api/products/[id]/fav.tsx
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/client/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({ // 해당 데이터 있는지 확인
+    where: {
+      productId: +id.toString(),
+      userId: user?.id,
+    },
+  });
+    let isHeart;
+  if (alreadyExists) {
+    await client.fav.delete({ // 이미 있다면 그 데이터를 지워야함
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
